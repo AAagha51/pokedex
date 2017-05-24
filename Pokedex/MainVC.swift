7d8df57a60f513cb98517d96c4ddfcff976f7735
Neuron-Mac <kami22m@gmail.com>
2017-05-24 15:27:06 +0500

    Added All Pokemon images

---
+++ Pokedex/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Neuron-Mac on 24/05/2017.
+//  Copyright © 2017 Aagha. All rights reserved.
+//
+
+import UIKit
+
+class MainVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
